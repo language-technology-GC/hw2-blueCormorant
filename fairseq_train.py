2335c6b3e847d2b8160f102c81c83c3f46a73bfa
ldelsignore <ldelsignore@fordham.edu>
2021-10-26 18:51:41 -0400

    Complete Part 3

--- fairseq_train.py
+++ fairseq_train.py
@@ -8,23 +8,28 @@ cmd = """ fairseq-train \
     --target-lang ice.p \
     --seed 1 \
     --arch lstm \
+    --dropout 0.2 \
+    --encoder-bidirectional \
+    --encoder-embed-dim 128 \
+    --decoder-embed-dim 128 \
+    --decoder-out-embed-dim 128 \
+    --encoder-hidden-size 512 \
+    --decoder-hidden-size 512 \
     --lr 0.001 \
     --optimizer adam \
     --clip-norm 1 \
     --keep-last-epochs -1 \
     --criterion label_smoothed_cross_entropy \
     --max-update 800 \
-    --encoder-bidirectional \
-    --encoder-embed-dim 128 \
-    --decoder-embed-dim 128 \
-    --batch-size 64
+    --batch-size 50 \
+    --label-smoothing 0.1
 """
 
 run(cmd, shell=True)  
 
 '''
+    --decoder-output-dim 128 \
     --required-batch-size-multiple 5 
-    --decoder-output-dim 128
 '''
 
 
