00cc7e57cbe7176f13d0506aa259d00146de8db6
ldelsignore <ldelsignore@fordham.edu>
2021-11-03 16:20:47 -0400

    Forgot to add preprocess

--- preprocess.py
+++ preprocess.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python
 
 with open("ice_train.tsv", "r") as in_file:
-    g_file = open("train.ice.g", "w")
-    devg_file = open("dev.ice.g", "w")
-    testg_file = open("test.ice.g", "w")
-    
-    p_file = open("train.ice.p", "w")
-    devp_file = open("dev.ice.p", "w")
-    testp_file = open("test.ice.p", "w")
+    with open("train.ice.g", "w") as gfile:
+        with open("train.ice.p", "w") as pfile:
+            for line in in_file:
+                word = line.split("\t")[0]
+                phones = line.split("\t")[1]
+                graphemes = " ".join(word)
+                gfile.write(graphemes+"\n")
+                pfile.write(phones)
 
-    for line in in_file:
-        word = line.split("\t")[0]
-        phones = line.split("\t")[1]
-        graphemes = " ".join(word)
-        g_file.write(graphemes+"\n")
-        devg_file.write(graphemes+"\n")
-        testg_file.write(graphemes+"\n")
-        p_file.write(phones)
-        devp_file.write(phones)
-        testp_file.write(phones)
+with open("ice_test.tsv", "r") as in_file:
+    with open("test.ice.g", "w") as gfile:
+        with open("test.ice.p", "w") as pfile:
+            for line in in_file:
+                word = line.split("\t")[0]
+                phones = line.split("\t")[1]
+                graphemes = " ".join(word)
+                gfile.write(graphemes+"\n")
+                pfile.write(phones)
 
-
-    g_file.close()
-    devg_file.close()
-    testg_file.close()
-    
-    p_file.close()
-    devp_file.close()
-    testp_file.close()
+with open("ice_dev.tsv", "r") as in_file:
+    with open("dev.ice.g", "w") as gfile:
+        with open("dev.ice.p", "w") as pfile:
+            for line in in_file:
+                word = line.split("\t")[0]
+                phones = line.split("\t")[1]
+                graphemes = " ".join(word)
+                gfile.write(graphemes+"\n")
+                pfile.write(phones)
 
